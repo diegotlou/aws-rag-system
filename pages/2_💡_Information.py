a6f7a56cd88584d29c2b67bbe320366b9e916270
Diego <diego13579dmc@gmail.com>
2026-06-03 23:43:04 -0600

    docs: add more information about the system

--- pages/2_💡_Information.py
+++ pages/2_💡_Information.py
@@ -6,7 +6,9 @@ st.markdown(
     """
     This chat bot is designed to assist users with questions related to Amazon EC2 (Elastic Compute Cloud). This Retrieval-Augmented Generation (RAG) system uses the next AWS public technical documents:
     * [Amazon EC2 User Guide](https://docs.aws.amazon.com/pdfs/AWSEC2/latest/UserGuide/ec2-ug.pdf)
-    * [Amazon EC2 Developer Guide](https://docs.aws.amazon.com/pdfs/ec2/latest/devguide/ec2-dg.pdf)\n
-    This chatbot does not remember previous questions or answers. Always verify the information received from the chatbot. 
+    * [Amazon EC2 Developer Guide](https://docs.aws.amazon.com/pdfs/ec2/latest/devguide/ec2-dg.pdf).\n
+    Here are some considerations you should keep in mind about this project:
+    - This chatbot does not remember previous questions or answers. Always verify the information received from the chatbot.
+    - The system has showed a average performance in robustness tests, with questions such as "my server keeps restarting by itself, how do i stop that??".
     """
 )
